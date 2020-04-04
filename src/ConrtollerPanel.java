e7b97cf920eb07bd66c02dc49679c47200a35c47
Mya-Mya <39019907+Mya-Mya@users.noreply.github.com>
2020-04-04 17:34:03 +0900

    位置指定と背景読み込みを追加・ボタン追加にてactionをより汎用的なものへ

--- src/ConrtollerPanel.java
+++ src/ConrtollerPanel.java
@@ -1,11 +1,14 @@
 import javax.swing.*;
+import javax.swing.filechooser.FileFilter;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.io.File;
 
 public class ConrtollerPanel extends JPanel {
     private TurtleView vTurtleView;
-    private Dimension buttonSize = new Dimension(100, 40);
+    private Dimension buttonSize = new Dimension(200, 40);
 
     public ConrtollerPanel(TurtleView vTurtleView) {
         super();
@@ -16,66 +19,119 @@ public class ConrtollerPanel extends JPanel {
         setForeground(UiFactory.white);
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
-        JLabel label = UiFactory.label();
-        label.setText("コントローラー");
-        label.setHorizontalTextPosition(JLabel.CENTER);
-        label.setAlignmentX(.5f);
-        add(label);
+        addComment("カメ");
 
-        addAction("直進", new Runnable() {
+        addActionButton("すすむ(↑)", KeyEvent.VK_UP,new ActionListener() {
             @Override
-            public void run() {
+            public void actionPerformed(ActionEvent e) {
                 vTurtleView.goStraight(20);
+                vTurtleView.updateUI();
             }
         });
 
-        addAction("右回り", new Runnable() {
+        addActionButton("右回り(→)",KeyEvent.VK_RIGHT, new ActionListener() {
             @Override
-            public void run() {
+            public void actionPerformed(ActionEvent e) {
                 vTurtleView.turn(45.);
+                vTurtleView.updateUI();
             }
         });
 
-        addAction("左回り", new Runnable() {
+        addActionButton("左回り(←)", KeyEvent.VK_LEFT,new ActionListener() {
             @Override
-            public void run() {
+            public void actionPerformed(ActionEvent e) {
                 vTurtleView.turn(-45.);
+                vTurtleView.updateUI();
             }
         });
 
-        addAction("大きく", new Runnable() {
+        addActionButton("大きく",null, new ActionListener() {
             @Override
-            public void run() {
+            public void actionPerformed(ActionEvent e) {
                 vTurtleView.larger(1.2);
+                vTurtleView.updateUI();
             }
         });
 
-        addAction("小さく", new Runnable() {
+        addActionButton("小さく",null, new ActionListener() {
             @Override
-            public void run() {
+            public void actionPerformed(ActionEvent e) {
                 vTurtleView.smaller(1.2);
+                vTurtleView.updateUI();
+            }
+        });
+
+        addActionButton("位置指定(c)",KeyEvent.VK_C, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                PositionAndAngleSetting setting=new PositionAndAngleSetting(
+                        vTurtleView.getTurtleX(),
+                        vTurtleView.getTurtleY(),
+                        vTurtleView.getAngle()
+                );
+                if(setting.isApproved()){
+                    vTurtleView.moveTo(setting.getTurtleX(),setting.getTurtleY());
+                    vTurtleView.setAngle(setting.getTurtleAngle());
+                    vTurtleView.updateUI();
+                }
+            }
+        });
+
+        addComment("背景");
+
+        Component parent = this;
+        addActionButton("読み込み",null, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser chooser = new JFileChooser(System.getProperty("user.dir"));
+                chooser.setFileFilter(new FileFilter() {
+                    @Override
+                    public boolean accept(File f) {
+                        return f.isDirectory()||
+                                f.getName().endsWith(".png") ||
+                                f.getName().endsWith(".jpg") ||
+                                f.getName().endsWith(".jpeg");
+                    }
+                    @Override
+                    public String getDescription() {
+                        return "背景にしたい写真を選んでね";
+                    }
+                });
+                if (chooser.showOpenDialog(parent) == JFileChooser.APPROVE_OPTION) {
+                    Image image = new ImageIcon(chooser.getSelectedFile().getAbsolutePath()).getImage();
+                    if (image == null) return;
+                    vTurtleView.setBackgroundImage(image);
+                    vTurtleView.updateUI();
+                }
             }
         });
 
         setVisible(true);
     }
 
-    private void addAction(String text, Runnable action) {
-
+    private void addComment(String text) {
         add(Box.createVerticalStrut(10));
 
+        JLabel label = UiFactory.label();
+        label.setText(text);
+        label.setHorizontalTextPosition(JLabel.CENTER);
+        label.setAlignmentX(.5f);
+        add(label);
+
+        add(Box.createVerticalStrut(10));
+    }
+
+    private void addActionButton(String text,Integer mnemonic, ActionListener action) {
         JButton button = UiFactory.button();
         button.setText(text);
         button.setAlignmentX(.5f);
         button.setMaximumSize(buttonSize);//BoxLayoutの時はMaximSizeで大きさを指定するんだって
-        button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                action.run();
-                vTurtleView.updateUI();
-            }
-        });
+        button.addActionListener(action);
+        if (mnemonic!=null) {
+            button.setMnemonic(mnemonic);
+        }
         add(button);
+        add(Box.createVerticalStrut(10));
     }
 
 }
