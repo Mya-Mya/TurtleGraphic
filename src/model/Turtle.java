ceae5076c71733138f870442bae636623fd59536
Mya-Mya <39019907+Mya-Mya@users.noreply.github.com>
2020-04-28 14:07:18 +0900

    xとyの変更は同時にしないと移動をアニメーション化できない

--- src/model/Turtle.java
+++ src/model/Turtle.java
@@ -17,17 +17,25 @@ public class Turtle {
     private double y;
 
     public Turtle(double angle, double size, double x, double y) {
-        setAngle(angle);
-        setX(x);
-        setY(y);
+        this.angle = angle;
+        this.size = size;
+        this.x = x;
+        this.y = y;
         setImage(new ImageIcon("img/turtle.png").getImage());
+        onTransformChanged(angle, angle, size, size, x, x, y, y);
     }
 
     public void addTurtleListener(TurtleListener l) {
         mTurtleListenerList.add(l);
     }
 
-    public void fireTurtleListeners(double angle0, double angle1, double size0, double size1, double x0, double x1, double y0, double y1) {
+    public void onImageChanged(Image image0, Image image1) {
+        for (TurtleListener l : mTurtleListenerList) {
+            l.onTurtleImageChanged(image0, image1);
+        }
+    }
+
+    public void onTransformChanged(double angle0, double angle1, double size0, double size1, double x0, double x1, double y0, double y1) {
         for (TurtleListener l : mTurtleListenerList) {
             l.onTurtleTransformChanged(angle0, angle1, size0, size1, x0, x1, y0, y1);
         }
@@ -56,28 +64,27 @@ public class Turtle {
     public void setAngle(double angle) {
         double angle0 = angle;
         this.angle = (angle + 360.) % 360.;
-        fireTurtleListeners(angle0, angle, size, size, x, x, y, y);
+        onTransformChanged(angle0, angle, size, size, x, x, y, y);
     }
 
     public void setImage(Image image) {
+        Image image0 = image;
         this.image = image;
+        onImageChanged(image0, image);
     }
 
     public void setSize(double size) {
         double size0 = size;
         this.size = size;
-        fireTurtleListeners(angle, angle, size0, size, x, x, y, y);
+        onTransformChanged(angle, angle, size0, size, x, x, y, y);
     }
 
-    public void setX(double x) {
+
+    public void setPosition(double x, double y) {
         double x0 = x;
-        this.x = x;
-        fireTurtleListeners(angle, angle, size, size, x0, x, y, y);
-    }
-
-    public void setY(double y) {
         double y0 = y;
+        this.x = x;
         this.y = y;
-        fireTurtleListeners(angle, angle, size, size, x, x, y0, y);
+        onTransformChanged(angle, angle, size, size, x0, x, y0, y);
     }
 }
