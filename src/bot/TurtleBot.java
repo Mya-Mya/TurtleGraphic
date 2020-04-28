304140082ec2742c760a83de12c94f906083d5a2
Mya-Mya <39019907+Mya-Mya@users.noreply.github.com>
2020-04-28 14:37:59 +0900

    命令の送信先を変更

--- src/bot/TurtleBot.java
+++ src/bot/TurtleBot.java
@@ -1,9 +1,9 @@
 package bot;
 
-import viewmodel.TurtleViewModel;
+import model.TurtleSimulator;
 
 public class TurtleBot {
-    public TurtleBot(TurtleViewModel turtle) {
+    public TurtleBot(TurtleSimulator turtle) {
         turtle.goStraight(200);
         turtle.turnRight();
         turtle.goStraight(50);
