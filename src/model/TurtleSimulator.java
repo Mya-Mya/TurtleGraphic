dd0a91f3e00c741dd3e52864d97f7e962a638664
Mya-Mya <39019907+Mya-Mya@users.noreply.github.com>
2020-04-28 14:43:30 +0900

    Created TurtleSimulator

---
+++ src/model/TurtleSimulator.java
@@ -0,0 +1,85 @@
+package model;
+
+import model.Turtle;
+import model.World;
+import model.floor.Floor;
+import model.floor.FloorColor;
+
+public class TurtleSimulator {
+    private Turtle mTurtle;
+    private World mWorld;
+
+    public TurtleSimulator(Turtle mTurtle, World mWorld) {
+        this.mTurtle = mTurtle;
+        this.mWorld = mWorld;
+    }
+
+    public double getSimulatedAngle() {
+        return mTurtle.getAngle();
+    }
+
+    public double getSimulatedSize() {
+        return mTurtle.getSize();
+    }
+
+    public double getSimulatedX() {
+        return mTurtle.getX();
+    }
+
+    public double getSimulatedY() {
+        return mTurtle.getY();
+    }
+
+    public Floor getFloorOn() {
+        return mWorld.getFloorOn(getSimulatedX(), getSimulatedY());
+    }
+
+    public FloorColor getFloorColorOn() {
+        Floor floor = getFloorOn();
+        if (floor != null) {
+            return floor.getFloorColor();
+        }
+        return null;
+    }
+
+    public void goStraight(double l) {
+        double theta = Math.toRadians(mTurtle.getAngle());
+        double x1 = mTurtle.getX() + l * Math.cos(theta);
+        double y1 = mTurtle.getY() + l * Math.sin(theta);
+        mTurtle.setPosition(x1, y1);
+    }
+
+    public boolean isOnAnyFloor() {
+        return getFloorColorOn() != null;
+    }
+
+
+    public void larger(double ratio) {
+        mTurtle.setSize(mTurtle.getSize() * ratio);
+    }
+
+    public void setAngle(double angle){
+        mTurtle.setAngle(angle);
+    }
+
+    public void setPosition(double x,double y){
+        mTurtle.setPosition(x,y);
+    }
+
+    public void smaller(double ratio) {
+        larger(1. / ratio);
+    }
+
+    public void turn(double deltaAngle) {
+        mTurtle.setAngle(mTurtle.getAngle() + deltaAngle);
+    }
+
+    public void turnLeft() {
+        turn(-90);
+    }
+
+    public void turnRight() {
+        turn(90);
+    }
+
+}
