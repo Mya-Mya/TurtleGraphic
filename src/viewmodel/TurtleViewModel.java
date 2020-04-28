1c6e8fe8e4f1c87e54f6cfbe6b1f5c50da5753c0
Mya-Mya <39019907+Mya-Mya@users.noreply.github.com>
2020-04-28 14:28:41 +0900

    TurtleBotからの受付をTurtleSimulatorへ委譲・ゲッターを新設・変数名の変更

--- src/viewmodel/TurtleViewModel.java
+++ src/viewmodel/TurtleViewModel.java
@@ -1,13 +1,15 @@
 package viewmodel;
 
+import model.Turtle;
+import model.TurtleListener;
+
 import java.awt.*;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Queue;
 
-public class TurtleViewModel {
-
+public class TurtleViewModel implements TurtleListener {
     interface AnimationFrame {
         void startRunning();
 
@@ -16,159 +18,65 @@ public class TurtleViewModel {
         void finalFrame();
     }
 
-    private List<TurtleViewModelListener> behaviourListenerList;
+    private List<TurtleViewModelListener> mTurtleViewModelListenerList;
 
     private Queue<AnimationFrame> animationFrameQueue;
 
-    private double x = 50;
-    private double y = 50;
-    private double angle = 0;
-    private double size = 1;
-    private Image iBackground;
+    private double angle;
+    private Image image;
+    private double size;
+    private double x;
+    private double y;
     private Boolean runningTurtleBotThread = false;
 
     private final int FREQUENCY = 40;
     private final double ALL_TIME = .3;
 
-    public TurtleViewModel() {
-        behaviourListenerList = new ArrayList<>();
+    public TurtleViewModel(Turtle mTurtle) {
+        angle = mTurtle.getAngle();
+        size = mTurtle.getSize();
+        image = mTurtle.getImage();
+        x = mTurtle.getX();
+        y = mTurtle.getY();
+
+        mTurtleViewModelListenerList = new ArrayList<>();
         animationFrameQueue = new ArrayDeque<>();
     }
 
     public void addTurtleBehaviourListener(TurtleViewModelListener listener) {
-        behaviourListenerList.add(listener);
+        mTurtleViewModelListenerList.add(listener);
         listener.onTurtleViewModelChanged();
     }
 
     private void fireTurtleBehaviourListener() {
-        for (TurtleViewModelListener listener : behaviourListenerList) {
+        for (TurtleViewModelListener listener : mTurtleViewModelListenerList) {
             listener.onTurtleViewModelChanged();
         }
     }
 
-    public void move(double deltaX, double deltaY) {
-        double halfPi = Math.PI / 2.;
-
-        addAnimation(new AnimationFrame() {
-            double startX;
-            double startY;
-
-            @Override
-            public void startRunning() {
-                startX = x;
-                startY = y;
-            }
-
-            @Override
-            public void run(double time) {
-                double sinTime = Math.sin(time * halfPi);
-                x = startX + sinTime * deltaX;
-                y = startY + sinTime * deltaY;
-            }
-
-            @Override
-            public void finalFrame() {
-                x = startX + deltaX;
-                y = startY + deltaY;
-            }
-        });
+    @Override
+    public void onTurtleTransformChanged(double angle0, double angle1, double size0, double size1, double x0, double x1, double y0, double y1) {
 
     }
 
-    public void moveTo(double x, double y) {
-        move(x - this.x, y - this.y);
+    @Override
+    public void onTurtleImageChanged(Image image0, Image image1) {
+        this.image = image1;
+        fireTurtleBehaviourListener();
     }
 
-    public void goStraight(double l) {
-        addAnimation(new AnimationFrame() {
-            double startX;
-            double startY;
-            double cosAngle;
-            double sinAngle;
 
-            @Override
-            public void startRunning() {
-                startX = x;
-                startY = y;
-                double radianAngle = Math.toRadians(angle);
-                cosAngle = Math.cos(radianAngle);
-                sinAngle = Math.sin(radianAngle);
-            }
-
-            @Override
-            public void run(double time) {
-                x = startX + cosAngle * l * time;
-                y = startY + sinAngle * l * time;
-            }
-
-            @Override
-            public void finalFrame() {
-                x = startX + cosAngle * l;
-                y = startY + sinAngle * l;
-            }
-        });
+    public double getAngle() {
+        return angle;
     }
 
-    public void turn(double deltaAngle) {
-        double halfPi = Math.PI / 2.;
-        addAnimation(new AnimationFrame() {
-            double startAngle;
-
-            @Override
-            public void startRunning() {
-                startAngle = angle;
-            }
-
-            @Override
-            public void run(double time) {
-                angle = startAngle + deltaAngle * Math.sin(time);
-            }
-
-            @Override
-            public void finalFrame() {
-                angle = startAngle + deltaAngle;
-            }
-        });
-
+    public Image getImage() {
+        return image;
     }
 
-    public void turnRight() {
-        turn(90);
+    public double getSize() {
+        return size;
     }
-
-    public void turnLeft() {
-        turn(-90);
-    }
-
-    public void setAngle(double angle) {
-        turn(angle - this.angle);
-    }
-
-    public void larger(double ratio) {
-        addAnimation(new AnimationFrame() {
-            double startSize;
-
-            @Override
-            public void startRunning() {
-                startSize = size;
-            }
-
-            @Override
-            public void run(double time) {
-                size = startSize * Math.pow(ratio, time);
-            }
-
-            @Override
-            public void finalFrame() {
-                size = startSize * ratio;
-            }
-        });
-    }
-
-    public void smaller(double ratio) {
-        larger(1. / ratio);
-    }
-
 
     public double getTurtleX() {
         return x;
@@ -176,23 +84,6 @@ public class TurtleViewModel {
 
     public double getTurtleY() {
         return y;
-    }
-
-    public double getAngle() {
-        return angle;
-    }
-
-    public void setBackgroundImage(Image iBackground) {
-        this.iBackground = iBackground;
-        fireTurtleBehaviourListener();
-    }
-
-    public Image getBackgroundImage() {
-        return iBackground;
-    }
-
-    public double getSize() {
-        return size;
     }
 
     public boolean isRunningTurtleBotThread() {
