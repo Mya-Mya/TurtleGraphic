531188b35d99febcfdf826ac87855800eaf68fd0
Mya-Mya <39019907+Mya-Mya@users.noreply.github.com>
2020-04-28 15:17:55 +0900

    コンパクトに

--- src/view/StatePanel.java
+++ src/view/StatePanel.java
@@ -23,15 +23,14 @@ public class StatePanel extends JPanel implements MousePositionListener, TurtleV
         setBackground(UiFactory.back);
         setForeground(UiFactory.white);
         setLayout(new FlowLayout(FlowLayout.LEFT, 10, 0));
-        setBorder(UiFactory.bigEmptyBorder());
 
         cMousePositionLabel = UiFactory.label();
         add(cMousePositionLabel);
 
-        cTurtleTransformLabel =UiFactory.label();
+        cTurtleTransformLabel = UiFactory.label();
         add(cTurtleTransformLabel);
 
-        cTurtleSimulatedTransformLabel =UiFactory.label();
+        cTurtleSimulatedTransformLabel = UiFactory.label();
         add(cTurtleSimulatedTransformLabel);
 
         setVisible(true);
@@ -41,23 +40,23 @@ public class StatePanel extends JPanel implements MousePositionListener, TurtleV
 
     @Override
     public void onMouseMoved(Point point) {
-        cMousePositionLabel.setText("マウス座標:"+point.x + ", " + point.y);
+        cMousePositionLabel.setText("マウス:" + point.x + ", " + point.y);
     }
 
     @Override
     public void onTurtleViewModelChanged() {
-        int x=(int) mTurtleViewModel.getTurtleX();
-        int y=(int) mTurtleViewModel.getTurtleY();
-        int angle=(((int) mTurtleViewModel.getAngle())+360)%360;
-        cTurtleTransformLabel.setText("カメ座標:"+x+","+y+" カメ角度:"+angle);
+        int x = (int) mTurtleViewModel.getTurtleX();
+        int y = (int) mTurtleViewModel.getTurtleY();
+        int angle = (((int) mTurtleViewModel.getAngle()) + 360) % 360;
+        cTurtleTransformLabel.setText("カメ:" + x + "," + y + "," + angle);
     }
 
     @Override
     public void onTurtleTransformChanged(double angle0, double angle1, double size0, double size1, double x0, double x1, double y0, double y1) {
-        int x=(int)x1;
-        int y=(int)y1;
-        int angle=(int)angle1;
-        cTurtleSimulatedTransformLabel.setText("カメ座標(シミュレーション):"+x+","+y+" カメ角度(シミュレーション):"+angle);
+        int x = (int) x1;
+        int y = (int) y1;
+        int angle = (int) angle1;
+        cTurtleSimulatedTransformLabel.setText("カメ(シミュレーション):" + x + "," + y + "," + angle);
 
     }
 
