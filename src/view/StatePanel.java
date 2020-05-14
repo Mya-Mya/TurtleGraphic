b3caa6de490c481b101650b81fbcfcad480e8aa7
Mya-Mya <interleukin413@gmail.com>
2020-05-14 20:55:16 +0900

    カメの情報を表示

--- src/view/StatePanel.java
+++ src/view/StatePanel.java
@@ -11,13 +11,14 @@ import javax.swing.*;
 import java.awt.*;
 
 public class StatePanel extends JPanel implements MousePositionListener, TurtleListener {
+    private Turtle mTurtle;
     private TurtleSimulator mTurtleSimulator;
     private JLabel cMousePositionLabel;
     private JLabel cTurtleTransformLabel;
-    private JLabel cTurtleSimulatedTransformLabel;
 
     public StatePanel(Turtle mTurtle, TurtleSimulator mTurtleSimulator) {
         super();
+        this.mTurtle = mTurtle;
         this.mTurtleSimulator = mTurtleSimulator;
 
         setBackground(UiFactory.back);
@@ -30,9 +31,6 @@ public class StatePanel extends JPanel implements MousePositionListener, TurtleL
         cTurtleTransformLabel = UiFactory.label();
         add(cTurtleTransformLabel);
 
-        cTurtleSimulatedTransformLabel = UiFactory.label();
-        add(cTurtleSimulatedTransformLabel);
-
         setVisible(true);
         mTurtle.addTurtleListener(this);
     }
@@ -42,38 +40,26 @@ public class StatePanel extends JPanel implements MousePositionListener, TurtleL
         cMousePositionLabel.setText("マウス:" + point.x + ", " + point.y);
     }
 
-/*
-    @Override
-    public void onTurtleTransformChanged(double angle0, double angle1, double size0, double size1, double x0, double x1, double y0, double y1) {
-        int x = (int) x1;
-        int y = (int) y1;
-        int angle = (int) angle1;
-        FloorColor floorColor = mTurtleSimulator.getFloorColorOn();
-        String onFloorText = "";
-        if (floorColor != null) {
-            onFloorText = floorColor.toString() + "の上";
-        }
-        cTurtleSimulatedTransformLabel.setText("カメ(シミュレーション):" + x + "," + y + "," + angle + " " + onFloorText);
+    private void updateTurtleTransformLabel() {
+        cTurtleTransformLabel.setText("カメ: " + (int) mTurtle.getX() + ", " + (int) mTurtle.getY() + ", " + (int) mTurtle.getAngle());
     }
- */
 
     @Override
     public void onTurtleAngleChanged(double angle) {
-
+        updateTurtleTransformLabel();
     }
 
     @Override
     public void onTurtleSizeChanged(double size) {
-
+        updateTurtleTransformLabel();
     }
 
     @Override
     public void onTurtlePositionChanged(double x, double y) {
-
+        updateTurtleTransformLabel();
     }
 
     @Override
     public void onTurtleImageChanged(Image image0, Image image1) {
-
     }
 }
